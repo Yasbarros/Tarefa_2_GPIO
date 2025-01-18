4fe63e5ca1df6dfe32cb95a3ed8ef07bbc6835f3
Gustavo Medeiros <carvalho02gustavo@gmail.com>
2025-01-18 09:47:06 -0300

    modificando de 2 segs para 0,5 segs

--- tarefa2_led_buzzer.c
+++ tarefa2_led_buzzer.c
@@ -1,5 +1,5 @@
 void activate_buzzer() {
     gpio_put(BUZZER_PIN, 1);  // Liga o buzzer
-    sleep_ms(2000);           // Aguarda 2 segundos
-    gpio_put(BUZZER_PIN, 0);  //Desliga o buzzer
+    sleep_ms(500);           // Aguarda 0,5 segs
+    gpio_put(BUZZER_PIN, 0);  // Desliga o buzzer
 }
