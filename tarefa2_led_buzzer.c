742645d0ba2244a36f7e92df1b519240b8771d9e
Gustavo Medeiros <carvalho02gustavo@gmail.com>
2025-01-17 23:52:24 -0300

    ativacao commit

--- tarefa2_led_buzzer.c
+++ tarefa2_led_buzzer.c
@@ -1,5 +1,5 @@
 void activate_buzzer() {
     gpio_put(BUZZER_PIN, 1);  // Liga o buzzer
     sleep_ms(2000);           // Aguarda 2 segundos
-    gpio_put(BUZZER_PIN, 0);  // Desliga o buzzer
+    gpio_put(BUZZER_PIN, 0);  //Desliga o buzzer
 }
