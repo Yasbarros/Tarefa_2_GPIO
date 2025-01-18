b52232500310640610eb04d9c7756f19994826a6
Yasmim <barrosyasmim13@gmail.com>
2025-01-17 22:57:12 -0300

    Função principal

---
+++ tarefa2_led_buzzer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>         // Adicionado para corrigir o erro de 'strncmp'
+#include "pico/stdlib.h"
+#include "hardware/watchdog.h" // Necessário para o uso de watchdog_reboot
+
+
+
+int main()
+{
+    stdio_init_all(); // Inicializa a UART
+    init_gpio();      // Configuração inicial dos GPIOs
+
+    printf("Sistema iniciado. Digite os comandos:\n");
+    printf("Para Ligar Led Verde, digite: verde\n");
+    printf("Para Ligar Led Vermelho, digite: vermelho\n");
+    printf("Para Ligar Led Azul, digite: azul\n");
+    printf("Para Ligar Led branco, digite: branco\n");
+    printf("Para Desligar todos os Leds, digite: off\n");
+    printf("Para Ligar Buzzer, digite: buzzer\n");
+
+
+    while (1) {
+        
+    }
+}
